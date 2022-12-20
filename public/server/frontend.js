5f6093d02cbd3558949ae643482faec872148681
raymond-git <52261775+raymond-git@users.noreply.github.com>
2022-12-20 01:36:40 -0800

    Added useState to dropdown menu

--- public/server/frontend.js
+++ public/server/frontend.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json()); // to parse JSON bodies
@@ -6,22 +6,38 @@ app.use(express.json()); // to parse JSON bodies
 // The value "*" allows any domain to make requests
 // The value "Content-Type" allows the Content type header to be included in the request. Fixed Post request
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 // Server sends this response to a client
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const { name, email } = req.body;
-  console.log(`${name}! You have successfully registerd your account with your email: ${email} `);
+  console.log(
+    `${name}! You have successfully registerd your account with your email: ${email} `
+  );
   // Do something with the form data here, like saving it to a database
-  res.send({ message: 'Success!' });
+  res.send({ message: "Success!" });
+});
+
+app.get("/login", (req, res) => {
+  const BreedValues = req.query.value;
+  const dogImageUrl = `https://dog.ceo/api/breed/${BreedValues}/images/random`;
+  fetch(dogImageUrl, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
 });
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-  
--- src/SignupForm.js
+++ src/SignupForm.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
+import React, { useEffect, useState, } from 'react';
 
+// import axios from 'axios';
 function SignupForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const[dogBreeds, setDogBreeds] = useState([]);
+  const [dogBreeds, setDogBreeds] = useState([]);
+  const [imageUrl, setImageUrl] = useState('');
 
   const handleNameChange = (event) => {
     setName(event.target.value);
@@ -14,58 +15,160 @@ function SignupForm() {
     setEmail(event.target.value);
   };
 
-  //Make a post request to the server frontend.js
+  // Make a post request to the server frontend.js
   async function handleSubmit(event) {
     event.preventDefault();
-      fetch("http://localhost:3002/login", {
-        method: "POST",
-        body: JSON.stringify({
-          name: name,
-          email: email,
-        }),
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-        },
-      })
+    fetch("http://localhost:3002/login", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
       .then((response) => response.json())
       .then((data) => {
-        console.log('Success:', data);
+        console.log("Success:", data);
       })
       .catch(() => {
-        console.error('Something is wrong with your server');
+        console.error("Something is wrong with your server");
       });
+  }
+
+
+//Create Dropdown Menu
+const fetchImage = async () => {
+  const dog = await fetch("https://dog.ceo/api/breeds/list/all");
+  const dogList = await dog.json();
+
+  //Get all of the object for the dogs
+  const dogNames = Object.keys(dogList.message);
+  setDogBreeds(dogNames);
+};
+
+useEffect(() => {
+  fetchImage();
+}, []);
+
+
+
+//Select Option and display image
+const SelectBreed = async (event) => {
+  const options = document.getElementById("dropdownmenu");
+  const BreedValues = options.value;
+  const dogImageUrl = `https://dog.ceo/api/breed/${BreedValues}/images/random`;
+  const data = await fetch(dogImageUrl);
+  const getDog = await data.json();
+
+  setImageUrl(getDog.message);
+
+  if (options != null) {
+    options.addEventListener("change", (event) => SelectBreed(event));
+  }
+
+  return () => {
+    if (options != null) {
+      options.removeEventListener("change", (event) => SelectBreed(event));
     }
+  };
+};
 
-  useEffect(() => {
-     async function FetchDogData() {
-       const dogBreed = await fetch("https://dog.ceo/api/breeds/list/all");
-       const data = await dogBreed.json();
+useEffect(() => {
+  SelectBreed();
+});
+
+
+  // Create a dropdown menu  
+  // useEffect(() => {
+  //    async function FetchDogData() {
+  //      const dog = await fetch("https://dog.ceo/api/breeds/list/all");
+  //      const dogList = await dog.json();
        
-       //Get all of the object for the dogs
-       const dogNames = Object.keys(data.message);
-       setDogBreeds(dogNames);
-      }
+  //      //Get all of the object for the dogs
+  //      const dogNames = Object.keys(dogList.message);
+  //      setDogBreeds(dogNames);
+  //     };
+  //    FetchDogData();
+  //   }, []);
 
-     FetchDogData();
 
-    }, []);
+    // useEffect(() => {
+    //   const SelectBreed = async (event) => {
+    //     const BreedValues = event.target.value;
+    //     const dogImageUrl = `https://dog.ceo/api/breed/${BreedValues}/images/random`;
+    //     const data = await fetch(dogImageUrl);
+    //     const getDog = data.json();
+
+    //     setImageUrl(getDog.message);
+    //   };
+
+    //   const options = document.getElementById("dropdownmenu");
+    //   if (options != null) {
+    //     options.addEventListener("change", SelectBreed);
+    //   }
+
+    //   return () => {
+    //     if (options != null) {
+    //       options.removeEventListener("change", SelectBreed);
+    //     }
+    //   };
+    // }, []);
+
+   
+    //Trigger change when press on a breed
+    // const options = document.getElementById('dropdownmenu');
+
+    //  if (options != null) {
+    //   options.addEventListener("change", async function () {
+    //     const BreedValues = this.value;
+    //     const dogImageUrl = `https://dog.ceo/api/breed/${BreedValues}/images/random`;
+    //     const data = await fetch(dogImageUrl)
+    //     const getDog = data.json();
+
+    //     if (options.value === BreedValues) {
+    //       const imageElement = document.createElement("img");
+    //       imageElement.src = getDog.message;
+        
+    //       //Get the id from <img id="contain-image" src=""></img> and then execute
+    //       const imgContainer = document.getElementById("contain-image");
+          
+    //       if (imgContainer.hasChildNodes()) {
+    //         imgContainer.removeChild(imgContainer.lastChild);
+    //       } else {
+    //         // Add imageElement.src source to <img id="contain-image" src=""></img>
+    //         // `.append()` basically add new elements to the end of an array. For example: let numbers = [1, 2, 3]; numbers.append(4); [1, 2, 3, 4]
+    //         imgContainer.append(imageElement);
+    //         document.getElementById("postImage").appendChild(imageElement);
+    //       }
+    //     }
+    //   });
+    //  }
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        Name:{" "} <input type="text" onChange={handleNameChange} name="name" value={name}
+        Name:{" "} <input type="text" name="name" value={name} onChange={handleNameChange} 
         />
       </label>
       <br />
       <label>
-        Email:{" "} <input type="email" onChange={handleEmailChange} name="email" value={email}
+        Email:{" "} <input type="email" value={email} onChange={handleEmailChange} name="email" 
         />
       </label>
       <br />
       <input type="submit" value="Submit"></input>
 
-      <select id="dropdownmenu">{dogBreeds.map((breed) => (<option key={breed.id} value={breed}>{breed}</option>))}</select>
+
+      {/* <option key={uniqueId} value={breed}>{breed}</option> 
+      This creates an option element with a key and value and set current elements to dogBreeds array [   ]*/}
+      <select id="dropdownmenu">{dogBreeds.map((breed, uniqueId) => 
+          (<option key={uniqueId} value={breed}>{breed}</option>))}
+      </select>
+
+      <img id="contain-image" src={imageUrl} style={{width: '300px', height: '300px', objectFit: 'cover'}} alt="dogImages"></img>
     </form>
   );
 }
