0d99b2e785bb1bafc6910f5ca4944d77b4921fbd
raymond-git <52261775+raymond-git@users.noreply.github.com>
2022-12-25 01:15:47 -0800

    Stored users info to mongoDB database

--- public/server/frontend.js
+++ public/server/frontend.js
@@ -1,24 +1,23 @@
-const MongoClient = require("mongodb").MongoClient;
-const mongoose = require('mongoose');
+const cors = require("cors");
 const express = require("express");
-const app = express();
-const cors = require('cors');
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false); // Add this if you don't want to use the old behavior 
+const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
-const User = require('./models/user'); // Access models folder/user.js file to use in this server side
-const bcrypt = require('bcrypt'); // Hash password for security reason
+const User = require("./models/user"); // Access models folder/user.js file to use in this server side
+const bcrypt = require("bcrypt"); // Hash password for security reason
+const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json()); // to parse JSON bodies
-app.use(bodyParser.urlencoded({extended: true}));
-
-// The value "*" allows any domain to make requests
-// The value "Content-Type" allows the Content type header to be included in the request. Fixed Post request
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Origin", "*"); // The value "*" allows any domain to make requests
+  res.header("Access-Control-Allow-Headers", "Content-Type"); // The value "Content-Type" allows the Content type header to be included in the request. Fixed Post request
   next();
 });
+
 
 // Set up the connection to the mongoDB database
 mongoose.connect("mongodb://localhost:27017").then(() => {
@@ -30,44 +29,58 @@ mongoose.connect("mongodb://localhost:27017").then(() => {
 
 
 // Check to see if connected to MongoDB database
-const url = "mongodb://localhost:27017"
+const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
-client.connect()
-.then(() => {
-  console.log('Connected to the database');
-})
-.catch((error) => {
-  console.log('Eror connecting to the database', error);
-})
-
-
-// Server sends user form request to the client
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  if (email && password != "") {
-    res.send({ message: "You have succesfully signed in!" });
-  } else {
-    res.send({ Error: "Please enter the information to sign in" });
-  }
-});
-
-
-app.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
-  //Hashed the user's password when signing up
-  const hashpassword = await bcrypt.hash(password, 10);
-  const user = new User({
-    email: email,
-    password: hashpassword,
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.log("Can not connect to the database", error);
   });
 
 
-  // Save user information to mongoDB database
-  user.save((error) => {
-    if (error) {
-      res.status(500).send(error);
+  app.post("/signup", async (req, res) => {
+    const { email, password } = req.body;
+    const hashpassword = await bcrypt.hash(password, 10); //Hashed the user's password when signing up
+    const user = new User({
+      email: email,
+      password: hashpassword,
+    });
+    user.save((error) => { // Save user information to mongoDB database
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.send("You have successfully registered your account");
+      }
+    });
+  });
+
+
+// Server sends user form request to the client
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const dbName = "test";
+  const db = client.db(dbName);
+  const collection = db.collection("signups");
+  collection.find({ email: email, password: password }).then((user) => {
+    if (user) {
+      bcrypt.compare(password, user.password).then((match) => {
+        if (match) {
+          res.status(200).json({
+            status: "You have successfully logged in",
+          });
+        } else {
+          res.status(401).json({
+            status: "Invalid email or password",
+          });
+        }
+      });
     } else {
-      res.send("You have successfully registered your account");
+      res.status(401).json({
+        status: "Invalid email or password",
+      });
     }
   });
 });
@@ -89,4 +102,3 @@ app.get("/dogbreed", (req, res) => {
       res.sendStatus(500);
     });
 });
-
