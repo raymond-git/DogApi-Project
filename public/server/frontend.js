658920141741e08eb21cb45c9312b21f85e5c06b
Raymond Huang <raymondhuang615@yahoo.com>
2023-01-19 17:19:45 -0800

    Change port number

--- public/server/frontend.js
+++ public/server/frontend.js
@@ -41,7 +41,7 @@ mongoose.connect(dbURI, {
 })
   .then(() => {
     console.log("MongoDB Connected");
-    const port = process.env.PORT || 3001;
+    const port = process.env.PORT || 80;
     app.listen(port, () => {
       console.log(`Server listening on port ${port}`);
     });
