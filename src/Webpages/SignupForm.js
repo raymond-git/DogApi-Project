1a9df5ad83f0209cdafc6ea6835c194bcffe907b
Raymond Huang <raymondhuang615@yahoo.com>
2023-01-19 16:25:15 -0800

    Change login/signup fetch

--- src/Webpages/SignupForm.js
+++ src/Webpages/SignupForm.js
@@ -23,7 +23,7 @@ function SignupForm() {
   // Make a post request to the server frontend.js
   async function handleSubmit(event) {
     event.preventDefault();
-    fetch("http://localhost:3002/signup", {
+    fetch("https://main.dn2x8ssek5rtm.amplifyapp.com/signup", {
       method: "POST",
       body: JSON.stringify({
         email: email,
