6e5c81a92df862e5b8eb4caded0fea95ec536918
Raymond Huang <raymondhuang615@yahoo.com>
2023-01-20 19:15:21 -0800

    change url

--- src/Webpages/SignupForm.js
+++ src/Webpages/SignupForm.js
@@ -23,7 +23,7 @@ function SignupForm() {
   // Make a post request to the server frontend.js
   async function handleSubmit(event) {
     event.preventDefault();
-    fetch("http://localhost:3002/signup", {
+    fetch("https://dogbrowsing.com/signup", {
       method: "POST",
       body: JSON.stringify({
         email: email,
