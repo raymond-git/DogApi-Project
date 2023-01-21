90c56d9895077dac123f625e1e55dccdfbd8d22f
Raymond Huang <raymondhuang615@yahoo.com>
2023-01-20 19:09:15 -0800

    Test

--- public/server/frontend.js
+++ public/server/frontend.js
@@ -26,13 +26,13 @@ app.use((req, res, next) => {
   next();
 });
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(cors()) // Use this after the variable declaration
 
 const dbURI = process.env.MONGODB_URI;
 const dbPassword = process.env.MONGODB_PASSWORD;
