270977c8d430bef91f5d7c187cea7d3b44b02ce7
Raymond Huang <raymondhuang615@yahoo.com>
2023-01-23 17:04:28 -0800

    run build

--- src/Webpages/LoginForm.js
+++ src/Webpages/LoginForm.js
@@ -30,7 +30,7 @@ function LoginForm() {
   //Make a post request to the server frontend.js
   async function handleSubmit(event) {
     event.preventDefault();
-      fetch("https://www.dogbrowsing.com/login", {
+      fetch("https://ea11-2601-645-c480-6b00-ac7e-c86-eff1-349f.ngrok.io/login", {
       method: "POST",
       body: JSON.stringify({
         email: email,
